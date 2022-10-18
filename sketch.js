0d83f99098aec7fcc6b759048bc743ea0bee8b56
Gustav Paragas <gep21b@fsu.edu>
2022-10-18 13:12:55 -0400

    Made some initials

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+let w = 400
+let h = 600
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(w, h);
 }
 
 function draw() {
   background(125);
+  fill("orange");
+  stroke ("white");
+  beginShape (TESS);
+  vertex (20,20);
+  vertex (80,20);
+  vertex (80,40);
+  vertex (40,40);
+  vertex (40,60);
+  vertex (80,60);
+  vertex (80,80);
+  vertex (20,80);
+  endShape(CLOSE);
+
+
 }
